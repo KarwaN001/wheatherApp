3937282fdc87775c45a3ca674ba3e9b49bf8dc8b
KarwaN001 <karwanusf1@gmail.com>
2025-08-10 07:15:38 -0700

    Add search functionality and UI elements to ViewController
    - Introduced IBOutlet connections for search field, weather icon, temperature label, and city label.
    - Implemented an IBAction for the search button to update the city label with the text from the search field.
    - Updated Main.storyboard to include a search button and adjusted layout for better usability.

--- wheatherApp/ViewController.swift
+++ wheatherApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SearchField: UITextField!
+    @IBOutlet weak var whatherIcon: UIImageView!
+    @IBOutlet weak var whetherTempreture: UILabel!
+    @IBOutlet weak var whetherCity: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func SearchButton(_ sender: UIButton) {
+//        print(SearchField.text!)
+        whetherCity.text = SearchField.text
+    }
+    
 }
 
