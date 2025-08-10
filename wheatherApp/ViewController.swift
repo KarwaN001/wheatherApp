28fd1a72559df2fce7b3f3db00e9a2341813adc1
KarwaN001 <karwanusf1@gmail.com>
2025-08-10 05:41:57 -0700

    Initial commit

---
+++ wheatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wheatherApp
+//
+//  Created by karwan Syborg on 10/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
