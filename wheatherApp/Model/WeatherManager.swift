a7c7440b8da7b4e404f525a9e3b448e4927dcd00
KarwaN001 <karwanusf1@gmail.com>
2025-08-11 15:16:46 -0700

    Add WeatherModel to encapsulate weather data and refactor WeatherManager

--- wheatherApp/Model/WeatherManager.swift
+++ wheatherApp/Model/WeatherManager.swift
@@ -40,34 +40,19 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            
             let id = decodedData.weather[0].id
-            let temp = decodedData.main.temp
             let name = decodedData.name
+            let temp = decodedData.main.temp
             let description = decodedData.weather[0].description
-            print(temp, name, description, getConditionName(conditionId: id))
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, description: description, temperature: temp)
+            
+            print(temp, name, description, weather.conditionName , id)
         } catch {
             print(error)
         }
     }
 
-    func getConditionName(conditionId: Int) -> String {
-        switch conditionId {
-        case 200...232:
-            return "cloud.bolt"
-        case 300...321:
-            return "cloud.drizzle"
-        case 500...531:
-            return "cloud.rain"
-        case 600...622:
-            return "cloud.snow"
-        case 701...781:
-            return "cloud.fog"
-        case 800:
-            return "sun.max"
-        case 801...804:
-            return "cloud.bolt"
-        default:
-            return "cloud"
-        }
-    }
+    
 }
